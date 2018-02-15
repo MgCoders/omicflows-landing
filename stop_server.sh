4104d0642e5daf5f93967ed86e6530787c1a697a
rsperoni <rsperoni@mgcoders.com>
2018-02-15 17:54:45 -0300

    tab

--- stop_server.sh
+++ stop_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #/bin/bash
 set -x
-docker stack rm --compose-file=/home/ubuntu/REPLACE_PROJECT_NAME-deploy/docker-compose.testing.yml REPLACE_PROJECT_NAME
+docker stack rm REPLACE_PROJECT_NAME
